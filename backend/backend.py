b1b37ff9883599bc4902d9cc7240976d2eefb173
ladomosiashvili12 <lado.mosiashvili.2007@gmail.com>
2026-05-29 13:20:20 +0400

    register

--- backend/backend.py
+++ backend/backend.py
@@ -1,2 +1,15 @@
-def say_hello(name):
-    print("Hello", name)
+import sqlite3
+
+conn = sqlite3.connect('registration.db')
+# conn.row_factory = sqlite3.Row
+
+cursor = conn.sursor()
+
+cursor.execute('''create table registratio(
+               id integer primary key autoincrement,
+               username Nvarchar(50),
+               email Nvarchar(100),
+               password Nvarchar(50),
+               )''')
+
+git add backend/backend.py
